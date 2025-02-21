2c92b980ec97e552590d27fa85adbaf6456b758a
awas79 <colemanedward79@gmail.com>
2025-02-21 13:35:08 +1100

    Finish letter report

--- main.py
+++ main.py
@@ -7,16 +7,26 @@ def count_letters(contents):
         else:
             character_count[letter] += 1
 
-    print(character_count)
+    return(character_count)
+
+def sort_on(dict):
+    return dict["num"]
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(len(file_contents.split()))
+    total_count = len(file_contents.split())
 
-    count_letters(file_contents)
+    chracter_count = count_letters(file_contents)
+    sorted_count = dict(sorted(chracter_count.items(), reverse=True, key=lambda item: item[1]))
 
-    
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{total_count} words found in the document")
 
+    for letters in sorted_count:
+        if letters.isalpha():
+            print(f"The '{letters}' character was found {chracter_count[letters]} times")
+
+    print("--- End report ---")
 
 main()
